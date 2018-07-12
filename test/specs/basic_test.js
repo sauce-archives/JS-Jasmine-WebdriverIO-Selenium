609fe4f00576a62c7323e10410766ad7797cda99
joshgrant <joshua.m.grant@gmail.com>
2018-07-12 16:26:32 -0400

    added samples of tests and updated .gitignore

--- test/specs/basic_test.js
+++ test/specs/basic_test.js
@@ -1,9 +1,19 @@
 describe('the basic page', () => {
 
+    beforeEach(() => {
+        browser.url("http://a.testaddressbook.com")
+    });
+
     it('should have a title', () => {
         const actual = browser.getTitle();
-        const expected = 'Totes wrong';
+        const expected = 'Address Book';
 
-        expect(actual).toEqual(expected);
+        expect(expected).toEqual(actual);
+    });
+
+    it('should have a navigation bar', () => {
+        let navbar = $('#navbar'); // navbar selector
+
+        expect(navbar.isVisible());
     });
 })
